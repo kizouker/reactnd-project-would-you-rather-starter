3d27d91bde2be573664a4ff80f0505df413b3566
Rickard Åberg <rickard.aaberg@gmail.com>
2020-01-08 14:22:42 +0100

    only one option can be voted for per person

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -6,42 +6,96 @@ class Answer extends React.Component{
   constructor(props){
     super(props);
     this.handleVote = this.handleVote.bind(this);
+    this.state = {
+      optionOne : 'true',
+    }
   }
   componentDidMount() {
     const { question } = this.props.location.state; 
+
+    // let user = this.props.authenticatedUser;
+    // let questions2 = this.props.questions;
+
+      //
+
   }
 
 handleVote = ( e) => {
-  let option = e.target.name;
-  let oppositOption;
+  let user = this.props.authenticatedUser;
+  let questions2 = this.props.questions;
 
+ 
+  // const {user, questions2} = this.props;
+  let option = e.target.name;
+
+  let optionOneVotes, optionTwoVotes;
+
+
+  // let oppositOption;
+
+
+  // what option is chosen?
   if (option === "optionOne"){
-    oppositOption = "optionTwo";
+    // oppositOption = "optionTwo";
+    this.setState({optionOne : true});
   } else{
-    oppositOption ="optionOne"
+    // oppositOption ="optionOne"
+    this.setState({optionOne : false});
   }
 
+  // get the id of the question
   let id = e.target.value;
-  let user = this.props.authenticatedUser;
-  let questions = this.props.questions;
+  // let user = this.props.authenticatedUser;
+  // let questions = this.props.questions;
 
+  console.log("user in handleVote", user);
+  console.log("question in handleVote", questions2);
+  
   if (!isEmpty(id) && !isEmpty(option) && !isEmpty(user)) {
-    let question = questions[id];
-    let _option = question[option];
-    let _v = _option.votes;
 
-    let _oppositOption = question[oppositOption];
-    let _v_o = _oppositOption.votes;
+    optionOneVotes = questions2[id]["optionOne"].votes;
+    optionTwoVotes = questions2[id]["optionTwo"].votes;
 
-    if (!(_v.includes(user)) && !(_v_o.includes(user))){
-      _v.push(user);
-      question[option].votes = _v;
-      this.props.dispatch(updateQuestion(id, question))
+    let question = questions2[id];
+    // let _option = question[option];
+    // let _v = _option.votes;
 
-    } else{
-      alert("You already voted for this option");
+    // let _oppositOption = question[oppositOption];
+    // let _v_o = _oppositOption.votes;
+
+
+    //   if (!(_v.includes(user)) && !(_v_o.includes(user))){
+// four cases
+/** if user already exists in A , but clicked on b, then add user to B ( and remove from A)
+if user already exists in B, but clicked on am then add user to A (and remove from B)
+
+if user already exists in A, but clicked on a, then do nothing
+if user already exists in B, but clicked on b, then do nothing
+ */
+    // the user voted for one of the options
+    if ((optionOneVotes.includes(user)) && !(optionTwoVotes.includes(user)) 
+          && option === "optionTwo"){
+      optionOneVotes.pop(user);
+      optionTwoVotes.push(user);
+      
+      question["optionOne"].votes = optionOneVotes;
+      question[option].votes = optionTwoVotes;
+      this.props.dispatch(updateQuestion(id, question));
+
+    } else if(optionTwoVotes.includes(user) && !(optionOneVotes.includes(user)) 
+        && option === "optionOne"){
+      optionOneVotes.push(user);
+      optionTwoVotes.pop(user);
+
+      question["optionTwo"].votes = optionTwoVotes;
+      question[option].votes = optionOneVotes;
+      this.props.dispatch(updateQuestion(id, question));
     }
+    
+    // _v.push(user);
+    //   question[option].votes = _v;
      
+      
     }
   }
   render (){
@@ -66,13 +120,20 @@ handleVote = ( e) => {
                             onClick={ (e) => this.handleVote(e)}> Vote 
                           </button>
                           Votes: {question.optionOne.votes.length} 
-                          </td>
+                          {this.state.optionOne && (<div> 
+                          <b> Choosen </b>
+                        </div>)}
+                        </td>
+                       
                         <td>   ...   or   ...   </td>
                         <td>{question.optionTwo.text}  
                           <button name="optionTwo" value={question.id}
                               onClick={ (e) => this.handleVote(e)}> Vote 
                             </button> 
-                            Votes: {question.optionTwo.votes.length}             
+                            Votes: {question.optionTwo.votes.length}  
+                            {!this.state.optionOne && (<div> 
+                              <b> Choosen </b>
+                          </div>)}           
                         </td> 
                     </tr>                 
                 </tbody>
