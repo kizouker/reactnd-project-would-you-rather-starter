2da78135bebb9e2ea1379d0b32345b990b952008
Rickard Åberg <rickard.aaberg@gmail.com>
2020-01-15 16:40:03 +0100

    # and % only showed on answered polls

--- src/components/List.js
+++ src/components/List.js
@@ -97,30 +97,39 @@ countNoVotesPerQuestion = () => {
                              <td>... {el.author} ...
                                 <img src={window.location.origin + users[el.author].avatarURL} 
                                   width="10%" height="10%"/>
-                                wonders if you, would you rather...
+                                 wonders if you, would you rather...
                               </td>
-                              <td>{el.optionOne.text}    
+
+                              {el.optionOne.text}    
+
+                              {!unanswered && (<td>
                                   Votes: {el.optionOne.votes.length} 
-                              </td>
-                              <td>
-                                Percentage: {this.percentagePerQuestion()[el.id].optionOne}
-                              </td>
+                                </td>)}
+                     
+                              {!unanswered && (<td>
+                                  Percentage: {this.percentagePerQuestion()[el.id].optionOne}
+                                </td>)}
+                          
                               <td>   ...   or   ...   </td>
-                              <td>{el.optionTwo.text}  
-                              Votes: {el.optionTwo.votes.length}
-                              </td>          
+
                               <td>
-                                Percentage: {this.percentagePerQuestion()[el.id].optionTwo}
+                               {el.optionTwo.text}   
                               </td>
-                              <td>
-                                <Link to={{
-                                  pathname : '/questions/' + el.id,   
-                                  state: {
-                                    question : el,
-                                  }
-                                  }}>| Answer Poll |
-                                </Link>
-                              </td>        
+
+                              {!unanswered && (<td>
+                                  Votes: {el.optionTwo.votes.length} 
+                                </td>)}
+                         
+                              {!unanswered && (<td>
+                                  Percentage: {this.percentagePerQuestion()[el.id].optionTwo}
+                                </td>)}
+
+                                {unanswered && (<td>
+                                    <Link to={{ pathname : '/questions/' + el.id,   
+                                      state: {
+                                        question : el,
+                                      }}}>| Answer Poll |</Link>
+                                </td>)}
                             </tr>
                           </tbody>)
                         }
