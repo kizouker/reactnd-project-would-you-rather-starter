c80c2fdfde19edfcdfa92164311f662393890049
Rickard Åberg <rickard.aaberg@gmail.com>
2020-02-10 22:12:19 +0100

    removed comments

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,21 +2,9 @@ import React from 'react';
 import Dropdown from 'react-dropdown'
 import 'react-dropdown/style.css'
 import { connect } from 'react-redux';
-import { handleInitialData } from '../actions/shared'
-import { fn_other_file, AuthButton, isEmpty } from './Shared'
+import { AuthButton, isEmpty } from './Shared'
 import { setAuthenticatedUser} from '../actions/authedUser'
 
-/**
- * 
- * what is the difference of using "state" or to use a class variable to
- * remember/persist the state of isAuthenticatedUser?
- * 
- * what is the life cycle of component state?
- * 
- * I know that redux state is more persistant?
- * But How about a state in a js class?
- * 
- */
 class Login extends React.Component{
     constructor(props){
         super(props);
@@ -28,7 +16,6 @@ class Login extends React.Component{
       this.handleClick = this.handleClick.bind(this)
     }
 
-    // sets the authedUser in the state
     _onSelectSetAuthUser = ( authenticatedUser ) => {
         this.setState({ authenticatedUser },
             () => console.log(`Option selected:`, this.state.authenticatedUser)
@@ -50,11 +37,6 @@ class Login extends React.Component{
     }
          
   handleClick = (e) => { 
-    /** vad jag vill är att statet om vi är inloggade ska 
-     * 1) finnas globalt => state/store finns det state.authUser
-     * 2) login/logout finns "globalt", går att importera
-     * 3) login/logout ska visas hela tiden...
-     */
         this.props.authenticatedUser ? this.signout() : this.authenticate();
     }  
     render (){
@@ -70,12 +52,10 @@ class Login extends React.Component{
                         label : user.name
                     }
                     optionsDyn.push(object);
-                    return optionsDyn; // TODO:
+                    return optionsDyn;
             })
         }
 
-        const defaultOption = optionsDyn[0]; //TODO: fix default option
-   
       return(<div className="Login">
                  <div className="component-title"></div>
                   <Dropdown options={optionsDyn} 
@@ -96,7 +76,6 @@ class Login extends React.Component{
 }
 
 const mapStateToProps = ( state ) => {
-    console.log("inside map state to props Login, state: ", state)
     return {
         users : state.users,
         authenticatedUser : state.authenticatedUser.authenticatedUser
@@ -104,13 +83,3 @@ const mapStateToProps = ( state ) => {
 }
 
 export default connect(mapStateToProps) (Login);
-
-/****
- * 
- * Problems I faced
- * 1) In _onSelectSetAuthUser = ( obj ) the  this.setState({authenticatedUser : 'tantra'}) is
- * not really set, or is postponed - however the same value is controlled by the dropdown
- * and there it is updated...
- * BUG IN COMP OR WRONG USE...SETSTATE SHOLD JUST USE THE VARIABLE
- * AND THEN THE VALUE IS SET TO ...+ */
-
